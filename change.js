a818d0be564b1b9f17ca4230114c76391d9a05e6
maarten <van_den_bergmaarten@hotmail.com>
2023-03-13 12:05:55 +0700

    first commit

--- change.js
+++ change.js
@@ -4,16 +4,18 @@ fs.readdir(".", (err, files) => {
   if (err) console.log(err)
   let counter = 0
   files.forEach((file) => {
+    // below we specify a search term in between the slahes
     if (file.match(/yt1s.com - /)) {
-      const achtersteDeel = file.replace("yt1s.com - ", "")
-      // const achtersteDeel = file.replace("yt1s.com - ", "")
+      const remainderOfFileName = file.replace("yt1s.com - ", "")
+      // above we specify what part of the file name we want to change
+      // in this case we replace "yt1s.com - " by nothing   ""  (empty quotes)
       counter++
 
-      console.log(achtersteDeel)
-      fs.rename(file, `${achtersteDeel}`, (err) => {
+      console.log(remainderOfFileName) // lists all the new file names
+      fs.rename(file, `${remainderOfFileName}`, (err) => {
         if (err) console.log(err)
       })
     }
   })
-  console.log(counter)
+  console.log(counter) // the amount of files changed
 })
